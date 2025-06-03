eb7464fe6c696be9d49da6fb8c40892e22bce990
jhapriya909 <thepriyajha8840@gmail.com>
2025-06-03 09:05:29 +0530

    d:\DSA-Question150\DSA_Question\Day_1que\OddEven.cpp

---
+++ Day_1que/OddEven.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number :";
+    cin >> n;
+
+    if (n % 2 != 0)
+    {
+        cout << "Odd Numbers";
+    }
+
+    else
+    {
+        cout << "Even Numbers";
+    }
+}
