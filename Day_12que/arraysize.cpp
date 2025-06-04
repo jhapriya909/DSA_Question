5fb5f173e5fdd5caf679c980eebcb7ad0c20f2ce
jhapriya909 <thepriyajha8840@gmail.com>
2025-06-04 09:05:49 +0530

    d:\DSA-Question150\DSA_Question\Day_12que\arraysize.cpp

---
+++ Day_12que/arraysize.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+//     int size;
+//     cin>>size;
+//     int arr[1000];
+    
+//     int arr[8] ={1,2,3,4};
+//      for(int i = 0; i<size; i++){
+//         cout<<arr[i] <<"  ";
+//      }
+//  return 0;
+
+
+
+//  int arr[5] = {1,2,3,4,5};
+//  float  a;
+//  cout<<sizeof(a) <<" ";
+
+//  int arr[5] = {1,2,3,4,5};   //sizeof array
+//  float  a;
+//  cout<<sizeof(arr) <<" ";
+
+
+
+int arr[5] = {1,2,3,4,5};   //one element array
+ float  a;
+ cout<<sizeof(arr)/sizeof(arr[1]) <<" ";
+
+
+
+
+
+}
