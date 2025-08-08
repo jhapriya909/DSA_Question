10bd58e247cb9b1bab4d5971c65c32c18f4e6d04
jhapriya909 <thepriyajha8840@gmail.com>
2025-08-08 18:10:43 +0530

    d:\DSA-Question150\DSA_Question\Day_18que\SumEvenNumRange.cpp

---
+++ Day_18que/SumEvenNumRange.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i , num, sum=0;
+    cout<<"Enter amy number:";
+    cin>>num;
+    for(int i = 1; i<=10; i++){
+        if(i%2==0){
+            cout<<i<<endl;
+            sum = sum + i;
+
+        }
+    }
+    cout<<"sum of even number ="<<sum <<endl;
+
+}
