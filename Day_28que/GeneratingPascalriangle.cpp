ee70a0e5880faaab9c07392a74da8d61683212cc
jhapriya909 <thepriyajha8840@gmail.com>
2025-08-17 21:44:56 +0530

    d:\DSA-Question150\DSA_Question\Day_28que\GeneratingPascalriangle.cpp

--- Day_28que/GeneratingPascalriangle.cpp
+++ Day_28que/GeneratingPascalriangle.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {
-    int rows;
-    cout << "Enter number of rows: ";
-    cin >> rows;
+int main()
+{
+    int n;
+    cin >> n; // last number in sequence (1..n)
 
-    for (int i = 0; i < rows; i++) {
-        int num = 1; // first number in a row is always 1
-        for (int j = 0; j <= i; j++) {
-            cout << num << " ";
-            num = num * (i - j) / (j + 1); // next value using formula
-        }
-        cout << endl;
+    vector<int> arr(n - 1);
+    for (int i = 0; i < n - 1; i++)
+    {
+        cin >> arr[i]; // sequence input
     }
 
+    vector<bool> present(n + 1, false);
+
+    // mark numbers present
+    for (int x : arr)
+    {
+        present[x] = true;
+    }
+
+    cout << "[";
+    bool first = true;
+    for (int i = 1; i <= n; i++)
+    {
+        if (!present[i])
+        {
+            if (!first)
+                cout << ", ";
+            cout << i;
+            first = false;
+        }
+    }
+    cout << "]";
     return 0;
 }
