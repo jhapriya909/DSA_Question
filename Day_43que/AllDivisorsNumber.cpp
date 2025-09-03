3e10f8fe6e4a97883fbfe95dc2855fd0ca5c9076
jhapriya909 <thepriyajha8840@gmail.com>
2025-09-03 22:04:39 +0530

    d:\DSA-Question150\DSA_Question\Day_43que\AllDivisorsNumber.cpp

---
+++ Day_43que/AllDivisorsNumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Divisors of " << n << " are: ";
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {   // if i divides n
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
